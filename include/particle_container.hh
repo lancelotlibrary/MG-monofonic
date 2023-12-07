47b0de0a50c91f4fa68d8de6110644942c264841
Oleksii Sokoliuk <104352184+singlefrequency@users.noreply.github.com>
2023-12-07 16:53:50 +0000

    Update particle_container.hh

--- include/particle_container.hh
+++ include/particle_container.hh
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
+#include <cstdint>
 
 #ifdef USE_MPI
 #include <mpi.h>
